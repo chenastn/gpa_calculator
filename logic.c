fc941998f7d81684507e29a01f0f7ea5199c285c
Deja Bao <dejabao@Dejas-MacBook-Air-2.local>
2024-07-17 19:36:22 -0400

    j

---
+++ logic.c
@@ -0,0 +1,38 @@
+//logic of calculator
+int enterCredits(void);
+char enterGrade(void);
+double calcGrade(int, char);
+int main(void)
+{ 
+    int credits;
+    char grade;
+    double gpa;
+    credits = enterCredits();
+    grade = enterGrade();
+    gpa = calcGrade(credits, grade);
+}
+
+    int enterCredits(void)
+{ 
+    int credits;
+    
+      scanf("%d", &credits);
+    
+    return(credits);
+    
+}
+
+    char enterGrade(void)
+{
+    char grade;
+
+        scanf("%c", &grade);
+
+    return(grade);
+
+}
+
+    double calcGrade(int credits, char grade)
+ {
+
+}
