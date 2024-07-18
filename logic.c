26bbf106d32d87651aa7621930fa52fa53c08092
Austin Chen <chenastn@gmail.com>
2024-07-18 15:00:51 -0400

    testing

--- logic.c
+++ logic.c
@@ -1,38 +1,35 @@
-//logic of calculator
+// calculator logic
+
 int enterCredits(void);
 char enterGrade(void);
 double calcGrade(int, char);
-int main(void)
-{ 
+
+int main(void) { 
     int credits;
     char grade;
     double gpa;
+
     credits = enterCredits();
     grade = enterGrade();
     gpa = calcGrade(credits, grade);
 }
 
-    int enterCredits(void)
-{ 
+int enterCredits(void) { 
     int credits;
     
-      scanf("%d", &credits);
+    scanf("%d", &credits);
     
     return(credits);
-    
 }
 
-    char enterGrade(void)
-{
+char enterGrade(void) {
     char grade;
 
-        scanf("%c", &grade);
+    scanf("%c", &grade);
 
     return(grade);
-
 }
 
-    double calcGrade(int credits, char grade)
- {
+    double calcGrade(int credits, char grade) {
 
 }
